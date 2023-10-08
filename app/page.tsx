dcb01434a411764e1d2509384fe88605cc569c47
Knut Kirkhorn <knutkirk@hotmail.com>
2023-10-08 14:40:20 +0200

    Add background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center p-24 justify-between">
+		<main className="flex min-h-screen flex-col items-center p-24 justify-between bg-gradient-to-b from-slate-100 to-slate-200">
 			<div className="flex flex-col items-center space-y-4">
 				<h1 className="text-4xl font-bold mb-5 bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-gray-500">Next.js Template</h1>
 				<div className="flex flex-col">
