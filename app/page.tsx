dd14a3f0a1585616e685be3c3e9a8a4be47a158c
Knut Kirkhorn <knutkirk@hotmail.com>
2023-10-08 23:26:47 +0200

    Show dropdown in header for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import CopyButton from '@/components/copy-button';
-import ThemeSwitcherButton from '@/components/theme-switcher-button';
-import {Button} from '@/components/ui/button';
-import {Github, Twitter} from 'lucide-react';
+import Header from '@/components/header';
 import React from 'react';
 
 const installCommand = 'npx create-next-app@latest -e https://github.com/knutkirkhorn/next-template';
@@ -9,29 +7,7 @@ const installCommand = 'npx create-next-app@latest -e https://github.com/knutkir
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center p-16 bg-gradient-to-b from-slate-100 to-slate-200 dark:from-gray-900 dark:to-black">
-			<header className="flex justify-end items-center w-full mb-8 sm:mb-0">
-				<nav className="flex space-x-4">
-					<ThemeSwitcherButton />
-					<Button asChild>
-						<a
-							href="https://github.com/knutkirkhorn/next-template"
-							rel="noopener noreferrer"
-							target="_blank"
-						>
-							<Github className="w-4 h-4" />
-						</a>
-					</Button>
-					<Button asChild>
-						<a
-							href="https://twitter.com/knutkirkhorn"
-							rel="noopener noreferrer"
-							target="_blank"
-						>
-							<Twitter className="w-4 h-4" />
-						</a>
-					</Button>
-				</nav>
-			</header>
+			<Header />
 			<div className="flex flex-col items-center space-y-4">
 				<h1 className="text-4xl font-bold text-center mb-5 bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-gray-500 dark:from-gray-500 dark:to-gray-200">Next.js Template</h1>
 				<div className="flex flex-col">
---
+++ components/header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Github, Twitter} from 'lucide-react';
+import ThemeSwitcherButton from './theme-switcher-button';
+import {Button} from './ui/button';
+import HeaderDropdown from './header-dropdown';
+
+export default function Header() {
+	return (
+		<header className="flex justify-end items-center w-full mb-8 sm:mb-0">
+			<HeaderDropdown />
+			<nav className="hidden sm:flex space-x-4">
+				<ThemeSwitcherButton />
+				<Button asChild>
+					<a
+						href="https://github.com/knutkirkhorn/next-template"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						<Github className="w-4 h-4" />
+					</a>
+				</Button>
+				<Button asChild>
+					<a
+						href="https://twitter.com/knutkirkhorn"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						<Twitter className="w-4 h-4" />
+					</a>
+				</Button>
+			</nav>
+		</header>
+	);
+}
