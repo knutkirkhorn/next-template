92b84deb7e55b359ac85b41a6510d282cfe8bd95
Knut Kirkhorn <knutkirk@hotmail.com>
2023-10-08 17:02:38 +0200

    Add GitHub repo link and Twitter link

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
 import CopyButton from '@/components/copy-button';
+import {Button} from '@/components/ui/button';
+import {Github, Twitter} from 'lucide-react';
 import React from 'react';
 
 const installCommand = 'npx create-next-app@latest -e https://github.com/knutkirkhorn/next-template';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center p-24 justify-between bg-gradient-to-b from-slate-100 to-slate-200">
+		<main className="flex min-h-screen flex-col items-center p-16 bg-gradient-to-b from-slate-100 to-slate-200">
+			<header className="flex justify-end items-center w-full mb-8 sm:mb-0">
+				<nav className="flex space-x-4">
+					<Button asChild>
+						<a
+							href="https://github.com/knutkirkhorn/next-template"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							<Github className="w-4 h-4" />
+						</a>
+					</Button>
+					<Button asChild>
+						<a
+							href="https://twitter.com/knutkirkhorn"
+							rel="noopener noreferrer"
+							target="_blank"
+						>
+							<Twitter className="w-4 h-4" />
+						</a>
+					</Button>
+				</nav>
+			</header>
 			<div className="flex flex-col items-center space-y-4">
 				<h1 className="text-4xl font-bold mb-5 bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-gray-500">Next.js Template</h1>
 				<div className="flex flex-col">
