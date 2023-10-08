5704b3b63f74684701fef4a771c544fbf710752c
Knut Kirkhorn <knutkirk@hotmail.com>
2023-10-08 11:18:05 +0200

    Tweak Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	experimental: {
+		typedRoutes: true,
+	},
+	eslint: {
+		dirs: ['app', 'components', 'lib'],
+	},
+};
 
 module.exports = nextConfig;
