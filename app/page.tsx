b9a8ef01c3dc540b19077d241e39d19215041bf3
Knut Kirkhorn <knutkirk@hotmail.com>
2025-03-07 15:08:40 +0100

    Add margin between title and command

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 				<h1 className="mb-5 bg-linear-to-r from-gray-800 to-gray-500 bg-clip-text text-center text-4xl font-bold text-transparent dark:from-gray-500 dark:to-gray-200">
 					Next.js Template
 				</h1>
-				<div className="flex flex-col">
+				<div className="flex flex-col mt-4">
 					<p className="mb-4">
 						To install the template, run the following command:
 					</p>
