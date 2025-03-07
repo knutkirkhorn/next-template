db5e048b2703664db07309a06b77e1f7642d13fb
Knut Kirkhorn <knutkirk@hotmail.com>
2025-03-07 14:44:01 +0100

    Migrate to Tailwind CSS v4

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ const installCommand =
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-slate-100 to-slate-200 px-16 py-4 dark:from-gray-900 dark:to-black">
+		<main className="flex min-h-screen flex-col items-center bg-linear-to-b from-slate-100 to-slate-200 px-16 py-4 dark:from-gray-900 dark:to-black">
 			<Header />
 			<div className="flex flex-col items-center space-y-4">
-				<h1 className="mb-5 bg-gradient-to-r from-gray-800 to-gray-500 bg-clip-text text-center text-4xl font-bold text-transparent dark:from-gray-500 dark:to-gray-200">
+				<h1 className="mb-5 bg-linear-to-r from-gray-800 to-gray-500 bg-clip-text text-center text-4xl font-bold text-transparent dark:from-gray-500 dark:to-gray-200">
 					Next.js Template
 				</h1>
 				<div className="flex flex-col">
