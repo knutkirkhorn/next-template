2851fe4a43ef7855bf02ed6a83e6ebc62a617fec
Knut Kirkhorn <knutkirk@hotmail.com>
2025-05-19 17:51:04 +0200

    Change style

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ const installCommand =
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center bg-linear-to-b from-slate-100 to-slate-200 px-16 py-4 dark:from-gray-900 dark:to-black">
+		<main className="flex min-h-screen flex-col items-center px-16 py-4 bg-background">
 			<Header />
 			<div className="flex flex-col items-center space-y-4">
-				<h1 className="mb-5 bg-linear-to-r from-gray-800 to-gray-500 bg-clip-text text-center text-4xl font-bold text-transparent dark:from-gray-500 dark:to-gray-200">
+				<h1 className="mb-5 text-foreground text-center text-4xl font-bold">
 					Next.js Template
 				</h1>
 				<div className="flex flex-col mt-4">
-					<p className="mb-4">
+					<p className="mb-4 text-foreground">
 						To install the template, run the following command:
 					</p>
 					<CopyCommandButton textToCopy={installCommand} />
