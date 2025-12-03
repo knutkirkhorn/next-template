6762a50dac50b1719dc8faa847de7cf03a7ae11f
Knut Kirkhorn <knutkirk@hotmail.com>
2025-12-03 20:14:40 +0100

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-	experimental: {
-		// NOTE: unable to use these when using the --turbo flag
-		// typedRoutes: true,
-		// ppr: true,
-	},
-	eslint: {
-		dirs: [
-			'app',
-			'components',
-			'db',
-			'drizzle',
-			'hooks',
-			'lib',
-			'tests',
-			// Manually include top level files that are not linted
-			'./auth.ts',
-			'./drizzle.config.ts',
-			'./env.ts',
-			'./eslint.config.mjs',
-			'./postcss.config.mjs',
-			'./prettier.config.mjs',
-			'./tailwind.config.ts',
-		],
-	},
+	typedRoutes: true,
 };
 
 export default nextConfig;
